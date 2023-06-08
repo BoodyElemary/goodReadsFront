b6e49412655a16979a7b19848b96b1f2a045d830
Ayaat <ayaatadel128@gmail.com>
2023-06-08 05:22:25 +0200

    AllBooks AllAuthors

--- src/components/pages/AllCategories.jsx
+++ src/components/pages/AllCategories.jsx
@@ -1,5 +1,6 @@
 import React ,{useEffect,useState}from 'react';
 import {AppAPI} from '../../API/axiosAPI';
+import { Card } from 'react-bootstrap';
 import { useNavigate, useParams} from "react-router-dom";
 function AllCategories() {
     const navigate =useNavigate();
@@ -24,29 +25,34 @@ function AllCategories() {
     };
     useEffect(()=>{ getAllCategories()},[]);
 
-    return (
-  <div className='container p-5 rounded'>
-     
-     <h1  className=''>BOOKS Categories</h1>
 
-        <section className='d-flex justify-content-around  gap-5 flex-wrap py-5'>
-        {categories.map((category ,index) => (
-                     <div key={category._id} className=' border border-primary rounded p-5 w-25 '>
-
-                        <h2 className='' onClick={()=>viewCategory(category._id)}>{category?.categoryName}</h2>
-                  
-                    </div>
-              
-                      ))}
-            
-        </section>
-
-
+return (
+    <div className='col-12 container'>
           
-      
-     </div>
-    )
+    <div className=' my-3 wrap w-100 '>
+    <h1 className='d-flex' > Books Categories</h1>
+    </div>
+ <div className="text-center my-1  d-flex flex-wrap justify-content-around gap-3 overflow-hidden " style={{ padding:"10px" ,position:"relative" }} >
+       {categories.map((category ,index) => {
+  return (
+  
 
+    <Card key={category._id} className="border border-primary rounded p-4 w-25 m-1  gap-2  overflow-hidden wrap" style={{ borderRadius:"50%"}}>
+        
+      <h2 className=' ' onClick={()=>viewCategory(category._id)}>{category?.categoryName}</h2>
+     
+    </Card>
+  
+  );
+  
+})}
+
+      
+ 
+</div>
+</div>
+
+  )
 }
 
         
